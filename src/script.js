b6a1a5ca963fb25b47a8c920b0693e2640645d22
2Vladislav4 <vladislav.dedyul@gmail.com>
2024-09-28 16:23:35 +0300

    feat: button activity

--- src/script.js
+++ src/script.js
@@ -2,22 +2,66 @@ const start_button = document.querySelector(".start-button");
 const startWrapper = document.querySelector(".start-wrapper");
 const quizWrapper = document.querySelector(".quiz-wrapper");
 const questionHeading = document.querySelector(".question-heading");
+const resultHeading = document.querySelector(".result-heading");
 
-function generateCorrectButton() {
+
+let overallCounter = 0; /*Создаем счетчики ответов и вопросов*/
+let correctCounter = 0;
+let wrongCounter = 0;
+
+let answer;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", countAnswers(answer, button));
+})
+
+function countAnswers(answer, button) {
+    return function () {
+        overallCounter += 1;
+
+        if (String(answer) === button.innerText) {
+            correctCounter += 1;
+        } else {
+            wrongCounter += 1;
+        }
+    }
+}
+
+function showResults() { /*Функция РЕЗУЛЬТАТОВ*/
+    quizWrapper.classList.add("hide");
+    startWrapper.classList.remove("hide");
+    resultHeading.classList.remove("hide");
+    resultHeading.innerText = `TOTAL: ${overallCounter}, CORRECT: ${correctCounter}, WRONG: ${wrongCounter}`; /*Выводит результаты на экран*/
+}
+
+
+function generateCorrectButton(answer) {
     const buttons = document.querySelectorAll(".answer-row button"); /*Создаем массив с кнопками*/
-    const index = getRandomInt(buttons.length); /*В промежутке от 0 до конца массива выбирает случайную кнопку*/
-    const correctButton = buttons[index];
-    correctButton.innerText = "CORRECT!";
 
     buttons.forEach((button) => {
-        button.addEventListener("click", generateQuestion)
+        button.addEventListener("click", generateQuestion); /*Кнопка нажата => генерируется вопрос*/
+        let value = getRandomInt(100);
+        while (value === answer) {
+            value = getRandomInt(100);
+        }
+        button.innerText = value;
+
+        button.classList.remove("correct-button");
+        button.classList.add("wrong-button");
+
     })
+    const index = getRandomInt(buttons.length); /*В промежутке от 0 до конца массива выбирает случайную кнопку*/
+    const correctButton = buttons[index];
+    correctButton.innerText = answer;
+    correctButton.classList.remove("wrong-button");
+    correctButton.classList.add("correct-button");
 }
 
 function startQuiz() {
     startWrapper.classList.add("hide"); /*Прячет стартовый экран, добавляя ему класс hide*/
     quizWrapper.classList.remove("hide"); /*Убирает свойство hide с quizWrapper'a, что показывает его на экран*/
     generateQuestion();
+    setTimeout(showResults, 10000);
 }
 
 function getRandomInt(max) {
@@ -34,8 +78,14 @@ function generateQuestion() {
         sign = "-";
     }
 
+    if (sign === "+") {
+        answer = number1 + number2;
+    } else {
+        answer = number1 - number2
+    }
+
     questionHeading.innerText = `${number1} ${sign} ${number2}`; /*Вывод результата (примера) на экран*/
-    generateCorrectButton() 
+    generateCorrectButton(answer) /*Вызывается функция generateCorrectButton и туда передается ответ*/
 }
 
 
